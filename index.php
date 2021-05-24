26a4f27b971353e5adb6082bc8467fa7a2a582dc
mario9881 <mario_9881@abv.bg>
2021-05-24 23:02:37 +0300

    Add All offers and Submit offer pages

---
+++ index.php
@@ -0,0 +1,74 @@
+<?php
+	$GLOBALS["dbConnection"] = new mysqli("localhost", "root", "", "jobsboard");
+
+	function arrayResultFromQuery($sql) {
+		$array = [];
+		if ($queryResult = $GLOBALS["dbConnection"]->query($sql)) {
+			while ($row = $queryResult->fetch_assoc()) {
+				$array[] = $row;
+			}
+		}
+		return $array;
+	}
+
+	$allOffers = arrayResultFromQuery(
+		"SELECT * FROM offers 
+		 JOIN companies 
+		 ON offers.companyName = companies.name"
+	);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Jobs</title>
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+
+	<link rel="stylesheet" href="./css/master.css">
+	<link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap" rel="stylesheet">
+</head>
+<body>
+	<div class="site-wrapper">
+		<header class="site-header">
+			<h1 class="site-title"><a href="#">Job Offers</a></h1>
+		</header>
+
+		<ul class="jobs-listing">
+			<?php foreach($allOffers as $offer) { ?>
+			
+				<li class="job-card">
+					<div class="job-primary">
+						<h2 class="job-title"><a href="#"><?php echo $offer["title"]; ?></a></h2>
+						<div class="job-meta">
+							<a class="meta-company" href="#"><?php echo $offer["companyName"]; ?></a>
+							<span class="meta-date">Posted 14 days ago</span>
+						</div>
+						<div class="job-details">
+							<span class="job-location"><?php echo $offer["location"];?></span>
+							<span class="job-type"><?php echo $offer["jobType"]; ?></span>
+						</div>
+					</div>
+					<div class="job-logo">
+						<div class="job-logo-box">
+							<img src="https://i.imgur.com/ZbILm3F.png" alt="">
+						</div>
+					</div>
+				</li>
+
+			<?php } ?>
+		</ul>
+		
+		<footer class="site-footer">
+			<p>Copyright 2021 | Developer links: 
+				<a href="/edits.html">Edits</a>,
+				<a href="/index.html">Home</a>,
+				<a href="/single.html">Single</a>
+			</p>
+		</footer>
+	</div>
+
+</body>
+</html>
---
+++ submit_offer.php
@@ -0,0 +1,58 @@
+<?php
+	$GLOBALS["dbConnection"] = new mysqli("localhost", "root", "", "jobsboard");
+
+	if(isset($_POST["submit-form"])) {
+		$title = $_POST["job-title"];
+		$companyName = $_POST["company-name"];
+		$salary = $_POST["salary"];
+		$jobType = $_POST["job-type"];
+
+		$GLOBALS["dbConnection"]->query(
+			"INSERT INTO offers (title, companyName, salary, jobType)
+			VALUES('$title', '$companyName', $salary, '$jobType')"
+		);
+
+		header("Location: submit_offer.php");
+	}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Submit Offer</title>
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+
+	<link rel="stylesheet" href="./css/master.css">
+	<link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap" rel="stylesheet">
+</head>
+<body>
+	<div class="site-wrapper">
+		<header class="site-header">
+			<h1 class="site-title"><a href="#">Job Offers</a></h1>
+		</header>
+
+		<ul class="jobs-listing">	
+            <li class="job-card">
+                <form method="post">
+                    <p>Job Title: <input type="text" name="job-title"></p>
+                    <p>Company Name: <input type="text" name="company-name"></p>
+                    <p>Job Type: <input type="text" name="job-type"></p>
+                    <p>Salary: <input type="number" name="salary"></p>
+					<p><input type="submit" name="submit-form" value="Submit offer"></p>
+                </form>
+            </li>
+		</ul>
+		
+		<footer class="site-footer">
+			<p>Copyright 2021 | Developer links: 
+				<a href="/edits.html">Edits</a>,
+				<a href="/index.html">Home</a>,
+				<a href="/single.html">Single</a>
+			</p>
+		</footer>
+	</div>
+
+</body>
+</html>
